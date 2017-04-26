04dce1234ff235fa88fced1e30cbb50396e5bf4a
Felix Wittwer <dev@felixwittwer.de>
2017-04-26 18:39:55 +0200

    Add config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+This is the configuration file of the decision database interface.
+Please define the path to the SQLite3 database that contains all the data below.
+*/
+
+$db_path = "./db.sqlite3";
+
+?>
