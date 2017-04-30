dc1875f7d0e57ecac7e43d5a83f3ab6273355fc1
Stephan Fischer <hallo@tocsin.de>
2017-04-30 21:54:23 +0200

    First version of design

--- index.php
+++ index.php
@@ -2,19 +2,74 @@
 include("config.php");
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="de">
     <head>
         <title>iFSR Decision Database</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,700" rel="stylesheet">
+        <link href="style.css" rel="stylesheet" />
     </head>
 
     <body>
         <header>
+            <h1><a href="/"><img src="logo_header.png" alt="iFSR Decision Database Browser" title="iFSR Decision Database Browser" /></a> Decision Database Browser</h1>
             <form action="search.php" method="get">
-                <p><input type="text" name="query" /></p>
-                <input type="submit" />
+                <input type="text" name="query" placeholder="Suche" />
             </form>
         </header>
 
-        <hr />
+        <div class="content">
+        <h2>Die neusten Beschlüsse</h2>
+            <?php
+            // open a read-only connection to the database and receive all matching rows
+            $db = new SQLite3($db_path, SQLITE3_OPEN_READONLY) or die('Unable to open database');
+            $smt = $db->prepare("SELECT * FROM decisions ORDER BY decision_id DESC LIMIT 5");
+            $result = $smt->execute();
+            ?>
+
+            <div class="decisionlist">
+                <?php
+                $returned_something = false;
+                while ($row = $result->fetchArray(1)) {
+                    $returned_something = true;
+                    // TODO: Add Link to the Detail View, shorten the text, add "more" link.
+                    ?>
+                    <article>
+                        <div class="heading"><h3><?php 
+                        if ($row['money_limit'] != NULL) { 
+                            print("Finanzrahmen ". $row['decision_id'] ." (". $row['money_limit'] ."€)"); 
+                        } else {
+                            print("Beschluss ". $row['decision_id']);
+                        } ?></h3>
+                        <?php if ($row['accepted'] == 1) {print('<div class="outcome decided">Angenommen</div>');} else {print('<div class="outcome rejected">Abgelehnt</div>');}?></div>
+                        <p class="date">Beschlossen am <?php if (strlen($row['link']) != 0) { print("<a href='". $row['link'] ."'>". $row['date'] ."</a>"); } else { print($row['date']); } ?>.</p>
+                        <p class="text"><?php print($row['text']); ?></p>
+                        <div class="meta">
+                            <p class="comment"><?php print($row['comment']); ?></p>
+                            <p class="votes" title="Stimmen: [dafür | dagegen | Enthaltungen]">[ <?php print('<span class="decided">'. $row['v_yes'] .'</span> | <span class="rejected">'. $row['v_no'] ."</span> | ". $row['v_neutral']); ?> ]</p>
+                        </div>
+                    </article>
+                    
+
+                <?php
+                }
+
+                if (!$returned_something) {
+                ?>
+
+                <article>
+                    <h2>No results</h2>
+                    <p class="heading">
+                        Uh oh! Seems like no decision matched your criteria.
+                    </p>
+                </article>
+
+                <?php
+                }
+                ?>
+        </div>
+
     </body>
 </html>
--- search.php
+++ search.php
@@ -8,67 +8,76 @@ if (!isset($_GET['query'])) {
 include("config.php");
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="de">
     <head>
-        <title>Search | iFSR Decision Database</title>
+        <title>Suche | iFSR Decision Database</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,700" rel="stylesheet">
+        <link href="style.css" rel="stylesheet" />
     </head>
 
     <body>
         <header>
+            <h1><a href="/"><img src="logo_header.png" alt="iFSR Decision Database Browser" title="iFSR Decision Database Browser" /></a> Decision Database Browser</h1>
             <form action="search.php" method="get">
-                <p><input type="text" name="query" value="<?php echo htmlspecialchars($_GET['query']); ?>" /></p>
-                <input type="submit" />
+                <input type="text" name="query" placeholder="Suche" value="<?php echo htmlspecialchars($_GET['query']); ?>" />
             </form>
         </header>
 
-        <hr />
+        <div class="content">
 
-        <?php
-        // open a read-only connection to the database and receive all matching rows
-        $db = new SQLite3($db_path, SQLITE3_OPEN_READONLY) or die('Unable to open database');
-        $escaped_query = SQLite3::escapeString($_GET['query']);
-        $smt = $db->prepare("SELECT * FROM decisions WHERE decision_id LIKE '%". $escaped_query ."%' OR text LIKE '%". $escaped_query ."%' OR comment LIKE '%". $escaped_query ."%' OR date LIKE '%". $escaped_query ."%' ORDER BY decision_id DESC");
-        $result = $smt->execute();
-        ?>
-
-        <div class="decisionlist">
             <?php
-            $returned_something = false;
-            while ($row = $result->fetchArray(1)) {
-                $returned_something = true;
-                // TODO: Add Link to the Detail View, shorten the text, add "more" link.
+            // open a read-only connection to the database and receive all matching rows
+            $db = new SQLite3($db_path, SQLITE3_OPEN_READONLY) or die('Unable to open database');
+            $escaped_query = SQLite3::escapeString($_GET['query']);
+            $smt = $db->prepare("SELECT * FROM decisions WHERE decision_id LIKE '%". $escaped_query ."%' OR text LIKE '%". $escaped_query ."%' OR comment LIKE '%". $escaped_query ."%' OR date LIKE '%". $escaped_query ."%' ORDER BY decision_id DESC");
+            $result = $smt->execute();
+            ?>
+
+            <div class="decisionlist">
+                <?php
+                $returned_something = false;
+                while ($row = $result->fetchArray(1)) {
+                    $returned_something = true;
+                    // TODO: Add Link to the Detail View, shorten the text, add "more" link.
+                    ?>
+                    <article>
+                        <div class="heading"><h2><?php 
+                        if ($row['money_limit'] != NULL) { 
+                            print("Finanzrahmen ". $row['decision_id'] ." (". $row['money_limit'] ."€)"); 
+                        } else {
+                            print("Beschluss ". $row['decision_id']);
+                        } ?></h2>
+                        <?php if ($row['accepted'] == 1) {print('<div class="outcome decided">Angenommen</div>');} else {print('<div class="outcome rejected">Abgelehnt</div>');}?></div>
+                        <p class="date">Beschlossen am <?php if (strlen($row['link']) != 0) { print("<a href='". $row['link'] ."'>". $row['date'] ."</a>"); } else { print($row['date']); } ?>.</p>
+                        <p class="text"><?php print($row['text']); ?></p>
+                        <div class="meta">
+                            <p class="comment"><?php print($row['comment']); ?></p>
+                            <p class="votes" title="Stimmen: [dafür | dagegen | Enthaltungen]">[ <?php print('<span class="decided">'. $row['v_yes'] .'</span> | <span class="rejected">'. $row['v_no'] ."</span> | ". $row['v_neutral']); ?> ]</p>
+                        </div>
+                    </article>
+                    
+
+                <?php
+                }
+
+                if (!$returned_something) {
                 ?>
+
                 <article>
-                    <p class="heading"><?php 
-                    if ($row['money_limit'] != NULL) { 
-                        print("Finanzrahmen ". $row['decision_id'] ." (". $row['money_limit'] ."€)"); 
-                    } else {
-                        print("Beschluss ". $row['decision_id']);
-                    } ?></p>
-                    <p class="outcome"><?php if ($row['accepted'] == 1) {print("Angenommen.");} else {print("Abgelehnt.");}?></p>
-                    <p class="text"><?php print($row['text']); ?></p>
-                    <p class="comment"><?php print($row['comment']); ?></p>
-                    <p class="votes">[ <?php print($row['v_yes'] ." | ". $row['v_no'] ." | ". $row['v_neutral']); ?> ]</p>
-                    <p class="date">Beschlossen am <?php if (strlen($row['link']) != 0) { print("<a href='". $row['link'] ."'>". $row['date'] ."</a>"); } else { print($row['date']); } ?>.
+                    <h2>No results</h2>
+                    <p class="heading">
+                        Uh oh! Seems like no decision matched your criteria.
+                    </p>
                 </article>
-                
 
-            <?php
-            }
+                <?php
+                }
+                ?>
 
-            if (!$returned_something) {
-            ?>
-
-            <article>
-                <p class="heading">
-                    Uh oh! Seems like no decision matched your criteria.
-                </p>
-            </article>
-
-            <?php
-            }
-            ?>
-
+            </div>
         </div>
     </body>
 </html>
