0a4213b4d621ff90894290aef9e8fb5778eba287
Felix Wittwer <dev@felixwittwer.de>
2018-03-23 08:38:42 +0100

    Reflect changes on data model in examples.

--- init.php
+++ init.php
@@ -19,9 +19,11 @@ $query = "CREATE TABLE IF NOT EXISTS decisions (
 $db->exec($query) or die('Create db failed');
 
 if (isset($_GET['dummydata'])) {
-    $db->exec("INSERT INTO decisions VALUES ( '2017/02', 'Der FSR Informatik beschließt hiermit, getestete Software abzuschaffen.', NULL, 'Geistreicher Kommentar', 13, 2, 1, 1, '20.12.2017', 'https://ifsr.de' )");
-    $db->exec("INSERT INTO decisions VALUES ( '2017/03', 'Der FSR Informatik beschließt hiermit, ausschließlich ungetestete Hardware anzuschaffen.', NULL, 'Dummer Kommentar', 8, 7, 2, 0, '24.12.2017', 'https://ifsr.de' )");
-    $db->exec("INSERT INTO decisions VALUES ( '2017/01', 'Matthias beantragt einen Finanzrahmen über 200,12€ zur Deckung seiner Hosting-Kosten.', 200.12, 'Keine Gegenrede.', 17, 0, 0, 1, '01.04.2017', 'https://github.com/fsr/matthias' )");
+    $db->exec("INSERT INTO decisions VALUES ( '2017/02', 'Der FSR Informatik beschließt hiermit, getestete Software abzuschaffen.', 'Muss halt.', NULL, 'Geistreicher Kommentar', 13, 2, 1, 1, '20.12.2017', 'https://ifsr.de' )");
+    $db->exec("INSERT INTO decisions VALUES ( '2017/03', 'Der FSR Informatik beschließt hiermit, ausschließlich ungetestete Hardware anzuschaffen.', '', NULL, 'Dummer Kommentar', 8, 7, 2, 0, '24.12.2017', 'https://ifsr.de' )");
+    $db->exec("INSERT INTO decisions VALUES ( '2017/01', 'Matthias beantragt einen Finanzrahmen über 200,12€ zur Deckung seiner Hosting-Kosten.', '', 200.12, 'Keine Gegenrede.', 17, 0, 0, 1, '01.04.2017', 'https://github.com/fsr/matthias' )");
+    // Mehrheitsbeschlüsse werden mit [0 | 0 | 0], aber `accepted` = 1 gekennzeichnet.
+    $db->exec("INSERT INTO decisions VALUES ( '2017/04', 'Der FSR Informatik beschließt hiermit, mehrheitlich zu beschließen.', 'Ist halt cool.', NULL, '', 0, 0, 0, 1, '30.12.2017', 'http://stura.link/rektor' )");          
 }
 
 $db->close();
